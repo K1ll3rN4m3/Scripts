e6a75d1e4f3ad96d4f4c760131b4720bff2bc0eb
K1ll3rN4m3 <84021555+K1ll3rN4m3@users.noreply.github.com>
2022-05-21 09:48:05 +0800

    Create BankSteal.lua

---
+++ BankSteal.lua
@@ -0,0 +1,39 @@
+local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+
+-- Title
+
+local DarkAngelWindow = Library:NewWindow("DarkAngel Hub")
+
+-- Sections
+
+local DarkAngel = DarkAngelWindow:NewSection("Generator")
+
+-- Buttons And Toggles
+
+DarkAngel:CreateDropdown("DropDown", {"Obfuscate 1", "Obfuscate 2", "Obfuscate3"}, 2, function()
+print("This Is Cant Do Anything XD")
+end)
+
+DarkAngel:CreateToggle("Deposit All Pets", function()
+print("This Is A Biggest Scam Ever")
+end)
+
+DarkAngel:CreateToggle("Deposit All Gems", function()
+print("You Are Gonna Get Scam XD")
+end)
+
+DarkAngel:CreateToggle("Crash Game", function()
+print("Almost Getting Scammed XD")
+end)
+
+DarkAngel:CreateToggle("Use Webhook", function()
+print("You Have No Bitches XDD")
+end)
+
+DarkAngel:CreateTextbox("Your Webhook", function()
+        print("This Cant Do Anything Too XD")
+end)
+
+DarkAngel:CreateButton("Copy Script", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/K1ll3rN4m3/K1ll3rN4m3/main/Nothin-_-", true))()
+end)
